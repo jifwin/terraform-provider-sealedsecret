a375286e356f2c4d7f6733a80af227980aaabd47
Grzegorz Pietrusza <gpietrusza@gmail.com>
2024-03-24 20:21:09 +0100

    cleanup part 1

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -3,7 +3,6 @@ package provider
 import (
 	"context"
 	"errors"
-	"github.com/akselleirv/sealedsecret/internal/git"
 	"github.com/akselleirv/sealedsecret/internal/k8s"
 	"github.com/akselleirv/sealedsecret/internal/kubeseal"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
@@ -15,12 +14,10 @@ const (
 	host                 = "host"
 	clientCertificate    = "client_certificate"
 	clientKey            = "client_key"
+	token                = "token"
 	clusterCaCertificate = "cluster_ca_certificate"
 	controllerName       = "controller_name"
 	controllerNamespace  = "controller_namespace"
-	gitStr               = "git"
-	gitlabStr            = "gitlab"
-	sealedSecretInGit    = "sealedsecret_in_git"
 )
 
 func Provider() *schema.Provider {
@@ -61,50 +58,6 @@ func Provider() *schema.Provider {
 					},
 				},
 			},
-			gitStr: {
-				Type:        schema.TypeList,
-				MaxItems:    1,
-				Optional:    true,
-				Description: "Git repository credentials to where the sealed secret should be stored.",
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						url: {
-							Type:        schema.TypeString,
-							Required:    true,
-							Description: "URL to the repository.",
-						},
-						username: {
-							Type:        schema.TypeString,
-							Required:    true,
-							Description: "Username to be used for the basic auth.",
-						},
-						token: {
-							Type:        schema.TypeString,
-							Required:    true,
-							Sensitive:   true,
-							Description: "Token to be used for the basic auth.",
-						},
-						sourceBranch: {
-							Type:        schema.TypeString,
-							Default:     "main",
-							Optional:    true,
-							Description: "Name of the branch to be used. If the branch does not exist it will be created.",
-						},
-						targetBranch: {
-							Type:        schema.TypeString,
-							Default:     "main",
-							Optional:    true,
-							Description: "Name of the branch that should be merged to. Gitlab value must be set to true in order to create a merge request.",
-						},
-						gitlabStr: {
-							Type:        schema.TypeBool,
-							Default:     false,
-							Optional:    true,
-							Description: "If set to true the provider will create a merge request from source branch to target branch. This is currently supported for Gitlab.",
-						},
-					},
-				},
-			},
 			controllerName: {
 				Type:        schema.TypeString,
 				Optional:    true,
@@ -120,8 +73,7 @@ func Provider() *schema.Provider {
 		},
 		ConfigureContextFunc: configureProvider,
 		ResourcesMap: map[string]*schema.Resource{
-			sealedSecretInGit:    resourceInGit(),
-			"sealedsecret_local": resourceLocal(),
+			"sealedsecret_local": resourceLocal(), //TODO: drop _local suffix
 		},
 	}
 }
@@ -130,31 +82,14 @@ type ProviderConfig struct {
 	ControllerName      string
 	ControllerNamespace string
 	Client              *k8s.Client
-	Git                 *git.Git
-	IsGitlabRepo        bool
 	PublicKeyResolver   kubeseal.PKResolverFunc
 }
 
+// TODO: context?
 func configureProvider(ctx context.Context, rd *schema.ResourceData) (interface{}, diag.Diagnostics) {
 	k8sCfg, ok := getMapFromSchemaSet(rd, kubernetes)
 	if !ok {
 		return nil, diag.FromErr(errors.New("k8s configuration is required"))
-	}
-	gitCfg, ok := getMapFromSchemaSet(rd, gitStr)
-	var g *git.Git
-	var isGitlab bool
-	if ok {
-		var err error
-		g, err = git.NewGit(ctx, gitCfg[url].(string), gitCfg[sourceBranch].(string), gitCfg[targetBranch].(string), git.BasicAuth{
-			Username: gitCfg[username].(string),
-			Token:    gitCfg[token].(string),
-		})
-		if err != nil {
-			return nil, diag.FromErr(err)
-		}
-		isGitlab = gitCfg[gitlabStr].(bool)
-	} else {
-		logDebug("skipping setting up git client since no config was provided")
 	}
 
 	c, err := k8s.NewClient(&k8s.Config{
@@ -174,8 +109,6 @@ func configureProvider(ctx context.Context, rd *schema.ResourceData) (interface{
 		ControllerName:      cName,
 		ControllerNamespace: cNs,
 		Client:              c,
-		Git:                 g,
-		IsGitlabRepo:        isGitlab,
 		PublicKeyResolver:   kubeseal.FetchPK(c, cName, cNs),
 	}, nil
 }
