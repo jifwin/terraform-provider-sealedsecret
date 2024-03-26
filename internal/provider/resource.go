66e656c6504ee11d15b8788aa02067479908842c
Grzegorz Pietrusza <gpietrusza@gmail.com>
2024-03-26 19:30:14 +0100

    add separate logic for read

--- internal/provider/resource.go
+++ internal/provider/resource.go
@@ -11,15 +11,19 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
 	"log"
+	"strconv"
+	"strings"
 	"time"
 )
 
 const (
-	name       = "name"
-	namespace  = "namespace"
-	secretType = "type"
-	data       = "data"
-	stringData = "string_data"
+	name         = "name"
+	namespace    = "namespace"
+	secretType   = "type"
+	data         = "data"
+	stringData   = "string_data" //TODO: remove?
+	yaml_content = "yaml_content"
+	public_key   = "public_key"
 )
 
 type SealedSecret struct {
@@ -38,45 +42,70 @@ type SealedSecret struct {
 func resourceLocal() *schema.Resource {
 	return &schema.Resource{
 		Description:   "Creates a sealed secret and store it in yaml_content.",
-		CreateContext: resourceCreateLocal,
-		ReadContext:   resourceCreateLocal,
-		UpdateContext: resourceCreateLocal,
-		DeleteContext: resourceDeleteLocal,
+		CreateContext: resourceCreate,
+		ReadContext:   resourceRead,
+		DeleteContext: resourceDelete,
 		Schema: map[string]*schema.Schema{
 			name: {
 				Type:        schema.TypeString,
 				Required:    true,
+				ForceNew:    true,
 				Description: "name of the secret, must be unique",
 			},
 			namespace: {
 				Type:        schema.TypeString,
 				Required:    true,
+				ForceNew:    true,
 				Description: "namespace of the secret",
 			},
 			secretType: {
 				Type:        schema.TypeString,
 				Optional:    true,
 				Default:     "Opaque",
+				ForceNew:    true,
 				Description: "The secret type (ex. Opaque). Default type is Opaque.",
 			},
 			data: {
 				Type:        schema.TypeMap,
 				Optional:    true,
 				Sensitive:   true,
+				ForceNew:    true,
 				Description: "Key/value pairs to populate the secret. The value will be base64 encoded",
 			},
-			"yaml_content": {
+			yaml_content: {
 				Type:        schema.TypeString,
 				Computed:    true,
 				Description: "The produced sealed secret yaml file.",
+			},
+			public_key: {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The key used for encryption",
 			},
 		},
 	}
 }
 
-func resourceCreateLocal(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+func resourceRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	provider := meta.(*ProviderConfig)
+	pk, err := provider.PublicKeyResolver(ctx)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	if d.HasChanges(name, namespace, secretType, data) || (formatPublicKeyAsString(pk) != d.Get("public_key")) {
+		return resourceCreate(ctx, d, meta)
+	} else {
+		return nil
+	}
+}
+func resourceCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	provider := meta.(*ProviderConfig)
 	filePath := d.Get(name).(string)
+	pk, err := provider.PublicKeyResolver(ctx)
+	if err != nil {
+		return diag.FromErr(err)
+	}
 
 	logDebug("Creating sealed secret for path " + filePath)
 	sealedSecret, err := createSealedSecret(ctx, provider, d)
@@ -88,11 +117,12 @@ func resourceCreateLocal(ctx context.Context, d *schema.ResourceData, meta inter
 	d.SetId(filePath)
 	d.Set(data, d.Get(data).(map[string]interface{}))
 	d.Set("yaml_content", string(sealedSecret))
+	d.Set("public_key", formatPublicKeyAsString(pk))
 
 	return nil
 }
 
-func resourceDeleteLocal(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+func resourceDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	d.SetId("")
 	return nil
 }
@@ -140,6 +170,11 @@ func createSealedSecret(ctx context.Context, provider *ProviderConfig, d *schema
 	}
 
 	return kubeseal.SealSecret(secret, pk)
+}
+
+// TODO: refactor
+func formatPublicKeyAsString(pk *rsa.PublicKey) string {
+	return strings.Join([]string{pk.N.String(), strconv.Itoa(pk.E)}, "::")
 }
 
 func logDebug(msg string) {
